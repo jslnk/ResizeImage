4a5dde45f3735f8bbce39831529bfdf7f0c159a7
JS <jslnk@hotmail.com>
2020-10-21 22:22:43 -0400

    Add project files.

---
+++ ImageConversion.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace ResizeImage
+{
+    class ImageConversion
+    {  //this resizes an image by percentage 
+        public static bool resizeImage1(String imageFilePath, int width, int height, string destinationPath, int quality)
+        {
+
+            System.Drawing.Image myImage = System.Drawing.Image.FromFile(imageFilePath);
+
+            int sourceWidth = myImage.Width;
+            int sourceHeight = myImage.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)width / (float)sourceWidth);
+            nPercentH = ((float)height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(myImage, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            myImage.Dispose();
+
+            return SaveJpeg(destinationPath, (Image)b, quality);
+        }
+
+
+
+
+        //it resize the image and if there is space, it will be coloured with white
+        public static bool resizeImage(string stPhotoPath, int newWidth, int newHeight, string destinationPath, int quality, Color backColour, bool hasBorder)
+        {
+            Image imgPhoto;
+            try
+            {
+                imgPhoto = Image.FromFile(stPhotoPath);
+            }
+            catch
+            {
+                return false;
+            }
+
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            //Consider vertical pics   
+            if (sourceWidth < sourceHeight)
+            {
+                int buff = newWidth;
+                newWidth = newHeight;
+                newHeight = buff;
+            }
+            int sourceX = 0,
+                sourceY = 0,
+                destX = 0,
+                destY = 0;
+            float nPercent = 0, nPercentW = 0, nPercentH = 0;
+            nPercentW = ((float)newWidth / (float)sourceWidth);
+            nPercentH = ((float)newHeight / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((newWidth - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((newHeight - (sourceHeight * nPercent)) / 2);
+            }
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+
+            //set the back ground color if there is any space..
+            //grPhoto.Clear(Color.White);
+            grPhoto.Clear(backColour);
+
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            grPhoto.DrawImage(imgPhoto, new Rectangle(destX, destY, destWidth, destHeight), new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight), GraphicsUnit.Pixel);
+
+            if (hasBorder)
+            {
+                grPhoto.DrawRectangle(new Pen(Brushes.Black, 2), new System.Drawing.Rectangle(0, 0, bmPhoto.Width, bmPhoto.Height));
+            }
+
+            grPhoto.Dispose();
+            imgPhoto.Dispose();
+
+            return SaveJpeg(destinationPath, (Image)bmPhoto, quality);
+        }
+
+
+
+
+        /// <summary> 
+        /// Saves an image as a jpeg image, with the given quality 
+        /// </summary> 
+        /// <param name="path">Path to which the image would be saved.</param> 
+        // <param name="quality">An integer from 0 to 100, with 100 being the 
+        /// highest quality</param> 
+        //public static void SaveJpeg(string path, Image img, int quality, int width, int height)
+        private static bool SaveJpeg(string path, Image img, int quality)
+        {
+            bool returnValue = false;
+
+            if (quality < 0 || quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+
+            // Encoder parameter for image quality 
+            EncoderParameter qualityParam =
+                new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            // Jpeg image codec 
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            try
+            {
+                img.Save(path, jpegCodec, encoderParams);
+                //20130514
+                img.Dispose();
+                returnValue = true;
+
+            }
+            catch (Exception e)
+            {
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+
+        /// <summary> 
+        /// Returns the image codec with the given mime type 
+        /// </summary> 
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+
+
+
+
+
+        //public static void resizeImage(string fileName, int width, int height)
+        //{
+        //    Image oldImage = Image.FromFile(fileName);
+        //    Image thumb = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb) ;
+        //    Graphics oGraphic = Graphics.FromImage(thumb);
+
+        //    oGraphic.CompositingQuality = CompositingQuality.HighSpeed;
+        //    oGraphic.SmoothingMode = SmoothingMode.HighSpeed;
+        //    oGraphic.InterpolationMode = InterpolationMode.Low;
+
+        //    Rectangle rect = new Rectangle(0, 0, width, height);
+        //    oGraphic.DrawImage(oldImage, rect);
+        //    thumb.Save(fileName, ImageFormat.Jpeg);
+
+        //}
+
+
+    }
+}
+
---
+++ ImageResizer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace ResizeImage
+{
+    /// <summary>
+    /// This is to resize image
+    /// </summary>
+    public static class ImageResizer
+    {
+        public static void ResizeMultipleImagesWithSuffix(string sourceFolder, string destFolder, int height, int width, string suffix)
+        {
+            DirectoryInfo folderInfor = new DirectoryInfo(sourceFolder);
+
+            foreach (FileInfo eachFileInfo in folderInfor.GetFiles())
+            {
+                string eachFile = eachFileInfo.Name.ToLower();
+                if (eachFile.Contains(".jpeg") || eachFile.Contains(".bmp") || eachFile.Contains(".dds") || eachFile.Contains(".dng") || eachFile.Contains(".gif") ||
+                                eachFile.Contains(".tif") || eachFile.Contains(".png") || eachFile.Contains(".psd") || eachFile.Contains(".jpg") ||
+                                 eachFile.Contains(".yuv") || eachFile.Contains(".thm") || eachFile.Contains(".tga") || eachFile.Contains(".pspimage"))
+                {
+                    ResizeSingleImageWithSuffix(eachFileInfo.FullName, destFolder, height, width, suffix);
+                }
+            }
+        }
+
+        public static void ResizeSingleImageWithSuffix(string filePath, string destinationFolder, int height, int width, string suffix)
+        {
+
+            string destImageFile = Path.Combine(destinationFolder, Path.GetFileNameWithoutExtension(filePath) + suffix + ".jpg");
+
+
+            Color newColor = Color.FromArgb(255, 255, 255);
+            //System.Drawing.Image img = System.Drawing.Image.FromFile(filePath);
+
+
+            if (filePath.ToUpper().Contains(".PDF"))
+            {
+                //GenerateThumbnail(filePath, destImageFile, width, height, false);
+            }
+            else
+            {
+                ImageConversion.resizeImage(filePath, width, height, destImageFile, 99, newColor, false);
+            }
+
+
+
+        }
+
+
+
+        private static bool GenerateThumbnail(string sImagePath, string sDestPath, int iWidth, int iHeight, bool hasBorder)
+        {
+            bool result = false;
+
+            Process p = new Process();
+            ProcessStartInfo pageStandardInfo = new ProcessStartInfo(@"C:\Program Files\ImageMagick-6.8.3-Q16\identify.exe");
+            // Redirect the output stream of the child process.
+            pageStandardInfo.UseShellExecute = false;
+            pageStandardInfo.CreateNoWindow = true;
+            pageStandardInfo.RedirectStandardOutput = true;
+            pageStandardInfo.RedirectStandardError = true;
+            pageStandardInfo.RedirectStandardInput = true;
+            pageStandardInfo.Arguments = " -format %n " + sImagePath;
+            p.StartInfo = pageStandardInfo;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            output = output.Replace("\r\n", "");
+            string excuteString;
+            if (output == "1")
+            {
+                if (hasBorder)
+                {
+                    excuteString = sImagePath + " -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " -bordercolor black -border 1x1  " + sDestPath;
+                }
+                else
+                {
+                    excuteString = sImagePath + " -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " " + sDestPath;
+                }
+
+            }
+            else
+            {
+
+                if (hasBorder)
+                {
+                    excuteString = sImagePath + "[0] -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " -bordercolor black -border 1x1  " + sDestPath;
+                }
+                else
+                {
+                    excuteString = sImagePath + "[0] -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " " + sDestPath;
+                }
+
+
+            }
+
+
+            //string excuteString = sImagePath + "[0] -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " " + sDestPath
+
+            ProcessStartInfo startInfo;
+            startInfo = new ProcessStartInfo("..\\ImageMagick\\ImageMagick-7.0.10-34-Q16-HDRI-x64-dll.exe");
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardInput = true;
+            startInfo.Arguments = excuteString;
+            //will hide the cmd window
+            try
+            {
+                System.Diagnostics.Process.Start(startInfo);
+                result = true;
+            }
+            catch { }
+
+            //string excuteString = sImagePath + "[0] -strip -background white -colorspace RGB -resize " + iWidth + "x" + iHeight + " -gravity center -extent " + iWidth + "x" + iHeight + " " + sDestPath;
+            //System.Diagnostics.Process.Start(@"C:\Program Files\ImageMagick-6.8.3-Q16\convert.exe", excuteString);
+
+            return result;
+
+        }
+
+    }
+}
+
+
+
+
